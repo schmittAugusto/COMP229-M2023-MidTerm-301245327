00d2c129669653204abe3ce2fcd20a0bb2f215a7
schmittAugusto <133159429+schmittAugusto@users.noreply.github.com>
2023-07-07 13:21:16 -0400

    Initial commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+   url: "mongodb+srv://schmittaugusto:Ap0RpLDHRO3fr17L@cluster0.cuxmla8.mongodb.net/?retryWrites=true&w=majority",
+
+};
